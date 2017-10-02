f5f4c797fc8cdd12f1e72e3a17907c04ae31dec1
shawndfl <localhost@localhost>
2017-10-01 20:24:51 -0400

    Add unit test

---
+++ tests/UnitTest1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MemoryManager;
+using System.Diagnostics;
+
+namespace tests
+{   
+
+    [TestClass]
+    public class UnitTest1
+    {
+        public MemoryCollection memory;
+
+        private Process getTestProcess()
+        {
+            Process[] processes = Process.GetProcesses();
+
+            foreach (Process process in processes)
+            {
+                if (process.ProcessName == "Project64")
+                {
+                    return process;
+                }
+            }
+            return null;
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Process p = getTestProcess();
+            Assert.IsNotNull(p);
+
+            memory = new MemoryCollection(p);
+
+            foreach (IntPtr pointer in memory)
+            {
+
+            }
+
+        }
+    }
+}
